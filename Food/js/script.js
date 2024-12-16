a963c261085a367fd137256c156ba49518125b22
VladimiKoroviakov <138722521+VladimiKoroviakov@users.noreply.github.com>
2024-12-16 22:05:11 +0100

    Update script.js

--- Food/js/script.js
+++ Food/js/script.js
@@ -678,7 +678,7 @@ function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass
         });
 	}
 
-	function showTabContent(i = 0) {
+	function showTabContent(i = 1) {
         tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
         tabs[i].classList.add(activeClass);
